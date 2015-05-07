9060d7b05523a912b4d76ecde6456ed153ec159a
Nick Pope <pope410211@gmail.com>
2015-05-07 16:06:28 -0400

    First part
    We will rock this.

---
+++ string-calculator.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect
+
+function toEnglish(word){
+    if ( word === "zero" ) {
+      return 0;
+    }
+    if ( word === "one" ) {
+      return 1;
+    }
+    if ( word === "two" ) {
+      return 2;
+    }
+    if ( word === "three" ) {
+      return 3;
+    }
+    if ( word === "four" ) {
+      return 4;
+    }
+    if ( word === "five" )  {
+      return 5;
+    }
+    if ( word === "six" ) {
+      return 6;
+    }
+    if (word === "seven" ) {
+      return 7;
+    }
+}
+
+expect(toEnglish("zero")).to.equal(0);
+expect(toEnglish("one")).to.equal(1);
+expect(toEnglish("two")).to.equal(2);
+expect(toEnglish("three")).to.equal(3);
+expect(toEnglish("four")).to.equal(4);
+expect(toEnglish("five")).to.equal(5);
+expect(toEnglish("six")).to.equal(6);
+expect(toEnglish("seven")).to.equal(7);
+expect(toEnglish("eight")).to.equal(8);
+expect(toEnglish("nine")).to.equal(9);
