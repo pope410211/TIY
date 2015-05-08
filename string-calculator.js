609e23ce269139265674f95233775f58d9e0b06b
Nick Pope <pope410211@gmail.com>
2015-05-07 22:40:43 -0400

    Test Fail
    Failed to initiate addition issue -
    found problem in source code.
    
    `if ( A === "zero", B === "four") {
        return 4;
    }`
    // Should fix the error (syntax/typing issue)

--- string-calculator.js
+++ string-calculator.js
@@ -51,13 +51,39 @@ function plus( A, B) {
   if ( A === "zero", B === "one" ) {
       return 1;
   }
+  if ( A === "zero", B === "two") {
+      return 2;
+  }
+  if ( A == "zero", "three") {
+      return 3;
+  }
+  if (A === "zero", "four") {
+      return 4;
+  }
+  if ( A === "zero", B === "five") {
+      return 5;
+  }
+  if ( A === "zero", B === "six") {
+      return 6;
+  }
+  if ( A === "zero", B === "seven") {
+      return 7;
+  }
+  if ( A === "zero", B === "eight") {
+      return 8;
+  }
+  if ( A === "zero", B === "nine") {
+      return 9;
+  }
 }
 
 expect(plus("zero", "zero")).to.equal(0);
 expect(plus("zero", "one")).to.equal(1);
-
-// expect(plus("zero", "two")).to.equal(2);
-// expect(plus("zero", "three")).to.equal(3);
-// expect(plus("zero", "four")).to.equal(4);
-// expect(plus("zero", "five"))to.equal(5);
-// expect(plus("zero", "six")).to.equal(6);
+expect(plus("zero", "two")).to.equal(2);
+expect(plus("zero", "three")).to.equal(3);
+expect(plus("zero", "four")).to.equal(4);
+expect(plus("zero", "five"))to.equal(5);
+expect(plus("zero", "six")).to.equal(6);
+expect(plus("zero", "seven")).to.equal(7);
+expect(plus("zero", "eight")).to.equal(8);
+expect(plus("zero", "nine")).to.equal(9);
