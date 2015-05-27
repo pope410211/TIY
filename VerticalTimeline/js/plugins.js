854a94c609dacec895cd4c6f389c34c605cfef7d
Nick Pope <pope410211@gmail.com>
2015-05-27 15:39:51 -0400

    flag in sand. HTML-CSS

--- VerticalTimeline/js/plugins.js
+++ VerticalTimeline/js/plugins.js
@@ -1,4 +1,4 @@
-// Avoid `console` errors in browsers that lack a console.
+// Avoid `console` errors in browsers that lack a console.(MSIE).
 (function() {
     var method;
     var noop = function () {};
