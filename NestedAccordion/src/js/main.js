57b3b4d5728efcc8ed931a35fae3fea9224daeda
Nick Pope <pope410211@gmail.com>
2015-07-23 22:51:19 -0400

    woohoo

--- NestedAccordion/src/js/main.js
+++ NestedAccordion/src/js/main.js
@@ -3,18 +3,19 @@
 //using jQuery
 
 $('.trigger').click(function(){ // I am calling the classOfAnElement , and telling it that when it is clicked to invoke this function().
-  console.log("Click me one more time");
+  console.log("Click me one more time"); // tracer bullet to make sure it works
   $(this).parent().children('div').toggleClass('content-close');
-  if {
-    $(this).removeClass('icon')
-  } else {
-    $(this).addClass('icon')
-  }
+// I am saying that this parent Elements childeren Elements need to toggle (like a switch) the class (which changes my css info).
+});
+
+$('.trigger').click(function(){
+  console.log("Hope this works");
+  $(this).toggleClass('icon');
 })
 
 
-
-
+// Work on 'this'...what is it 'this' really saying?
+// I would still like to maybe go over it in Vanilla JS or break it down a little more.
 
 
 
