84cad6a74933d12304944e747bc331a7b0863a1d
Nick Pope <pope410211@gmail.com>
2015-07-23 22:42:56 -0400

    work please

--- NestedAccordion/src/js/main.js
+++ NestedAccordion/src/js/main.js
@@ -4,7 +4,12 @@
 
 $('.trigger').click(function(){ // I am calling the classOfAnElement , and telling it that when it is clicked to invoke this function().
   console.log("Click me one more time");
-  this.parent().child('div').toggleClass('content-close');
+  $(this).parent().children('div').toggleClass('content-close');
+  if {
+    $(this).removeClass('icon')
+  } else {
+    $(this).addClass('icon')
+  }
 })
 
 
