1234b655ee3ece022bb75bed077b5742e649c18c
Nick Pope <pope410211@gmail.com>
2015-07-23 22:35:22 -0400

    not opening

--- NestedAccordion/src/js/main.js
+++ NestedAccordion/src/js/main.js
@@ -3,8 +3,8 @@
 //using jQuery
 
 $('.trigger').click(function(){ // I am calling the classOfAnElement , and telling it that when it is clicked to invoke this function().
-  console.log("I'm clicking");
-
+  console.log("Click me one more time");
+  this.parent().child('div').toggleClass('content-close');
 })
 
 
