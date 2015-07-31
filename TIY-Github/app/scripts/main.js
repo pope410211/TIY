110f64e0a31dd3533be2f3683e0f3e6a91755571
Nick Pope <pope410211@gmail.com>
2015-07-31 12:47:42 -0400

    working in pulling data

--- TIY-Github/app/scripts/main.js
+++ TIY-Github/app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+(function(){
+
+var self = this;
+
+$.getJSON('api/users/octocat/main/octocatmain.json')
+.success(function(data){
+self.user = data;
+console.log('success', data);
+}).
+error(function(data){
+  console.log('error');
+
+});
+
+
+
+
+})(window);
