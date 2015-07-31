b2f1c6b05f4309ae2b09910dc161fdfe2792fc83
Nick Pope <pope410211@gmail.com>
2015-07-31 14:30:12 -0400

    data works

--- TIY-Github/app/scripts/main.js
+++ TIY-Github/app/scripts/main.js
@@ -1,16 +1,44 @@
 (function(){
 
-var self = this;
 
-$.getJSON('api/users/octocat/main/profile.json')
-.success(function(data){
-self.user = data;
-console.log('success', data);
-}).
-error(function(data){
-  console.log('error');
 
-});
+$.getJSON('api/users/' +  + '/main/profile.json')
+.then(function(user){
+console.log('success', user);
+
+$avatar = $('#avatar');
+$avatar.attr('src', user.avatar_url);
+
+$name = $('#user-name');
+$name.text(user.name);
+
+$login = $('#user-login');
+$login.text(user.login);
+
+$company = $('#company');
+$company.text(user.company);
+
+$location = $('#location');
+$location.text(user.location);
+
+$mail = $('#mail');
+$mail.text(user.email);
+$mail.attr('href', 'mailto:' + user.email);
+
+$blog = $('#blog');
+$blog.text(user.blog);
+$blog.attr('href', user.blog);
+
+$starred = $('#starred');
+$starred.text(user.starred_url.length);
+
+$followers = $('#followers');
+$followers.text(user.followers);
+
+$following = $('#following');
+$following.text(user.following);
+
+}); //end getJSON
 
 
 
