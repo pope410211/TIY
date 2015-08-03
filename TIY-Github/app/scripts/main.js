92fe12a503b25149d06764e1c121785e628cec60
Nick Pope <pope410211@gmail.com>
2015-08-03 12:45:13 -0400

    I will get this

--- TIY-Github/app/scripts/main.js
+++ TIY-Github/app/scripts/main.js
@@ -8,12 +8,32 @@
 // })
 
 
-var options = {};
-var profile = $('#profile-change option').each(function(){
-  options[$(this).text()] = $(this).val();
-});
+// var options = {};
+// var profile = $('#profile-change option').each(function(){
+//   options[$(this).text()] = $(this).val();
+// });
 
 // loop + if/else statement!
+// if(profile === 'octocat'){
+//   return 'octocat';
+//   console.log(profile);
+// } else {
+//   return 'pope410211';
+//   console.log(profile);
+// };
+
+$(document).ready( function ()
+{
+  /* we are assigning change event handler for select box */
+	/* it will run when selectbox options are changed */
+	$('#profile-change').change(function()
+	{
+		/* setting currently changed option value to option variable */
+		var option = $(this).find('option:selected').val();
+		/* setting input box value to selected option value */
+		$('#showoption').val(option);
+  });
+});
 
 console.log('yay', options, profile);
 
